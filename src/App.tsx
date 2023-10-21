6073bb4bfa71755ec9a8e7517c1509f052c75ee4
jenleap <jenleap@gmail.com>
2023-10-21 12:45:30 -0400

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+function App() {
+  return (
+    <Box sx={{ width: '50%', margin: "auto" }}>
+      <Typography variant="h1" sx={{ textAlign: "center"}}>
+        My Lists
+      </Typography>
+    </Box>
+  );
+}
+
+export default App;
